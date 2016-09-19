be7de49b1815725217cb6b0a7739e4bca26558fa
r3b311i0n <nasashinega@gmail.com>
2016-09-19 23:45:30 +0530

    scroll on click initial

--- app.js
+++ app.js
@@ -7,27 +7,35 @@ var main = function () {
     $(".about img").addClass("animated bounceInRight");
 };
 
-var pulseAnim = function (obj) {
+var _pulseAnim = function (obj) {
     $(obj).addClass("animated infinite");
 };
 
-var stopAnim = function (obj) {
+var _stopAnim = function (obj) {
     $(obj).removeClass("animated infinite");
 };
 
-var about = function (obj) {
+var _about = function (obj) {
+    $(".navhead p").removeClass("animated infinite flash");
+    $(obj).addClass("animated infinite flash");
+
+    scrollToBegin();
+};
+
+var _portfolio = function (obj) {
     $(".navhead p").removeClass("animated infinite flash");
     $(obj).addClass("animated infinite flash");
 };
 
-var portfolio = function (obj) {
+var _contact = function (obj) {
     $(".navhead p").removeClass("animated infinite flash");
     $(obj).addClass("animated infinite flash");
 };
 
-var contact = function (obj) {
-    $(".navhead p").removeClass("animated infinite flash");
-    $(obj).addClass("animated infinite flash");
+var scrollToBegin = function () {
+    $("html, body").animate({
+        scrollTop: $("#top").offset().top
+    }, 300);
 };
 
 $(document).ready(main);
